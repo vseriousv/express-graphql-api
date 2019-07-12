62400ff4fe26bd9621d5a760a1592614e9ed0ad9
kirill <gavrilovskirill@gmail.com>
2019-07-12 15:25:42 +0700

    Подключил mongoDB через Mongoose

--- schema/schema.js
+++ schema/schema.js
@@ -2,28 +2,11 @@ const graphql = require('graphql');
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt, GraphQLList} = graphql;
 
-// const users =  [
-    // {id: 1, firstName: 'Bran', lastName: 'Stark'},
-    // {id: 2, firstName: 'John', lastName: 'Snow'},
-    // {id: 3, firstName: 'Briena', lastName: 'Tart'},
-    // {id: 4, firstName: 'Daineris', lastName: 'Targarien'},
-    // {id: 5, firstName: 'Batya', lastName: 'Dragons'}
-// ];
-
-// const articles =  [
-//     {id: 1, title: 'Немного о главном', description: 'Пару слов о главном', numberLastDate: 50, authorID: 1},
-//     {id: 2, title: 'Статья два', description: 'Важная статья', numberLastDate: 32, authorID: 1},
-//     {id: 3, title: 'Еще одна статья', description: 'Это третья статья', numberLastDate: 28, authorID: 2},
-//     {id: 4, title: '4 статьи это уже норм', description: 'Нормально так 4 статьи', numberLastDate: 20, authorID: 2},
-//     {id: 5, title: 'Ну и на этом мы закончим', description: 'Последняя статья номер 5', numberLastDate: 5, authorID: 3},
-//     {id: 6, title: 'а вот нет не закончим', description: 'Еще одна статья после последней', numberLastDate: 5, authorID: 4},
-//     {id: 7, title: 'Давайте уже заканчивать статья 7', description: 'седьмая', numberLastDate: 5, authorID: 4},
-//     {id: 8, title: 'Ну закончили или нет?!', description: 'фух, уже восемь статей', numberLastDate: 5, authorID: 5},
-//     {id: 9, title: 'номер девять', description: 'ну и наконец последняя', numberLastDate: 5, authorID: 5}
-// ];
+const Users = require('../models/users');
+const Articles = require('../models/articles');
 
 const UsersType = new GraphQLObjectType({
-    name: 'users',
+    name: 'Users',
     fields: () => ({
         id: {type: GraphQLID},
         firstName: {type: GraphQLString},
@@ -31,23 +14,24 @@ const UsersType = new GraphQLObjectType({
         articles: {
             type: new GraphQLList(ArticlesType),
             resolve(parent, args) {
-                return articles.filter(article => article.authorID == parent.id);
+                //return articles.filter(article => article.authorID == parent.id);
+                return Articles.find({ authorID: parent.id });
             }
         }
     })
 });
 
 const ArticlesType = new GraphQLObjectType({
-    name: 'articles',
+    name: 'Articles',
     fields: () => ({
         id: {type: GraphQLID},
         title: {type: GraphQLString},
         description: {type: GraphQLString},
-        namberLastDate: {type: GraphQLInt},
-        authorID: {
+        author: {
             type: UsersType,
             resolve(parent, args){
-                return users.find(user => user.id == parent.id)
+               // return users.find(user => user.id == parent.id)
+               return Users.findById(parent.authorID);
             }
         }
     }),
@@ -60,26 +44,26 @@ const Query = new GraphQLObjectType({
             type: UsersType,
             args: { id: { type: GraphQLID } },
             resolve(parent, args) {
-                return users.find(user => user.id == args.id);
+                return Users.findById(args.id);
             }
         },
         article: {
             type: ArticlesType,
             args: { id: { type: GraphQLID } },
             resolve(parent, args) {
-                return articles.find(article => article.id == args.id);
+                return Articles.findById(args.id);
             }
         },
         users: {
             type: new GraphQLList(UsersType),
             resolve(parent, args) {
-                return users;
+                return Users.find({});
             }
         },
         articles: {
             type: new GraphQLList(ArticlesType),
             resolve(parent, args) {
-                return articles;
+                return Articles.find({});
             }
         }
     })
--- server/app.js
+++ server/app.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('../schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = 9000;
+
+mongoose.connect('mongodb://vseriousv:social123@localhost:27017/aidaprint', { useMongoClient: true });
 
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true
 }));
+
+const dbConnection = mongoose.connection;
+dbConnection.on('error', err => console.log(`Connection error: ${err}`));
+dbConnection.once('open', () => console.log('Connection to DB!'));
 
 app.listen(PORT, err => {
     err ? console.log(error) : console.log('Server started!');

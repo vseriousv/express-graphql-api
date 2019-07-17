fe1f59e319e4a1049a31b90553c503a8171d520b
kirill <gavrilovskirill@gmail.com>
2019-07-17 13:03:04 +0700

    Добавил Cors Libs

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('../schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 const PORT = 9000;
@@ -12,6 +13,8 @@ app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true
 }));
+
+app.use(cors());
 
 const dbConnection = mongoose.connection;
 dbConnection.on('error', err => console.log(`Connection error: ${err}`));

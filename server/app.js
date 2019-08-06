aa79a921f762d351c80a2d09b9454a11acae9da8
vseriousv <gavrilovskirill@gmail.com>
2019-08-06 10:05:36 +0700

    Изменил порт на 9000

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
-const PORT = 3001;
+const PORT = 9000;
 
 mongoose.connect('mongodb://vseriousv:social123@localhost:27017/aidaprint', { useNewUrlParser: true });
 
